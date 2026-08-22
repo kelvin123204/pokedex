339737e3462ccb98641bd8b4a8847e5dc78df371
kelvin123204 <kelvin123204@gmail.com>
2026-08-22 17:28:02 +0100

    add health endpoint

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
 
 	http.ListenAndServe(":8080", mux)
 }
